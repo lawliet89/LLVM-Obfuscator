cb78b74a76cd8b334450fb18025a2ca861f4a2c9
Yong Wen Chua <lawliet89@gmail.com>
2014-04-01 18:40:38 +0100

    Basic Block cloning and remapping

--- lib/Transform/boguscf.cpp
+++ lib/Transform/boguscf.cpp
@@ -1,10 +1,14 @@
 #include <algorithm>
 #include <vector>
 #include "llvm/Pass.h"
+#include "llvm/Transforms/Utils/Cloning.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/CFG.h"
+
+#define DEBUG_TYPE "boguscf"
 
 using namespace llvm;
 
@@ -22,32 +26,78 @@ struct BogusCF : public FunctionPass {
 
   virtual bool runOnFunction(Function &F) {
     bool hasBeenModified = false;
-    // TODO: Paramaterise these numbers
-    int max_bogus_blocks = 10;
-
     // If the function is declared elsewhere in other translation unit
     // we should not modify it here
     if (F.isDeclaration()) {
       return false;
     }
-
-    DEBUG(F.viewCFG());
+    DEBUG_WITH_TYPE("opt", errs() << "bcf: Function '" << F.getName() << "'\n");
 
     // Use a vector to store the list of blocks for probabilistic
     // splitting into two bogus control flow for a later time
     std::vector<BasicBlock *> blocks;
     blocks.reserve(F.size());
 
+    DEBUG_WITH_TYPE("opt", errs() << "\t" << F.size()
+                                  << " basic blocks found\n");
     for (Function::iterator B = F.begin(), BEnd = F.end(); B != BEnd; ++B) {
       blocks.push_back((BasicBlock *)B);
     }
 
-    std::random_shuffle(blocks.begin(), blocks.end());
+    // std::random_shuffle(blocks.begin(), blocks.end());
+    unsigned i = 0;
+    for (BasicBlock *block : blocks) {
+      DEBUG_WITH_TYPE("opt", errs() << "\tBlock " << i << "\n");
+      DEBUG_WITH_TYPE("opt", errs() << "\t\tSplitting Basic Block\n");
+      BasicBlock::iterator inst1 = block->begin();
+      if (block->getFirstNonPHIOrDbgOrLifetime()) {
+        inst1 = block->getFirstNonPHIOrDbgOrLifetime();
+      }
+      BasicBlock *originalBlock = block->splitBasicBlock(inst1);
 
-    int bogus_blocks = 0;
-    while (bogus_blocks < max_bogus_blocks && !blocks.empty()) {
-      break;
+      DEBUG_WITH_TYPE("opt", errs() << "\t\tCloning Basic Block\n");
+      Twine prefix = "cloned";
+      ValueToValueMapTy VMap;
+      BasicBlock *copyBlock = CloneBasicBlock(originalBlock, VMap, prefix, &F);
+
+      // Remap operands, phi nodes, and metadata
+      DEBUG_WITH_TYPE("opt", errs() << "\t\tRemapping information\n");
+      for (auto &inst : *copyBlock) {
+        // Operands
+        for (auto operand = inst.op_begin(), operandEnd = inst.op_end();
+             operand != operandEnd; ++operand) {
+          Value *v = MapValue(*operand, VMap);
+          if (v)
+            *operand = v;
+        }
+        // Phi nodes
+        if (PHINode *phi = dyn_cast<PHINode>(&inst)) {
+          for (unsigned i = 0, e = phi->getNumIncomingValues(); i != e; ++i) {
+            Value *v = MapValue(phi->getIncomingBlock(i), VMap);
+            if (v)
+              phi->setIncomingBlock(i, cast<BasicBlock>(v));
+          }
+        }
+
+        // Metadata
+        SmallVector<std::pair<unsigned, MDNode *>, 4> metas;
+        inst.getAllMetadata(metas);
+        for (auto &meta : metas) {
+          MDNode *oldMeta = meta.second;
+          MDNode *newMeta = MapValue(oldMeta, VMap);
+          if (oldMeta != newMeta)
+            inst.setMetadata(meta.first, newMeta);
+        }
+      }
+
+      // Create Opaque Predicate
+      // Always true for now
+
+
+      ++i;
     }
+
+    // DEBUG(F.viewCFG());
 
     return hasBeenModified;
   }
