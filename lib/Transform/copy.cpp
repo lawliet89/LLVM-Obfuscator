27a9b3a13c95c51bfefdf2f48bb92d734f689be3
Yong Wen Chua <lawliet89@gmail.com>
2014-05-01 20:05:57 +0100

    Basic cloning pass

--- lib/Transform/copy.cpp
+++ lib/Transform/copy.cpp
@@ -32,7 +32,7 @@ static cl::list<std::string> copyFunc("copyFunc", cl::CommaSeparated,
                                                "copyFunc=\"func1,func2\""));
 
 static cl::opt<double> copyProbability(
-    "copyProbability", cl::init(0.2),
+    "copyProbability", cl::init(0.5),
     cl::desc(
         "Probability that a function will be copied if list is not specified"));
 
@@ -63,6 +63,7 @@ bool Copy::runOnModule(Module &M) {
 
   bool hasBeenModified = false;
   auto funcListStart = copyFunc.begin(), funcListEnd = copyFunc.end();
+  std::vector<Function *> cloneList;
   for (auto &F : M) {
     if (F.isDeclaration())
       continue;
@@ -71,20 +72,49 @@ bool Copy::runOnModule(Module &M) {
     if (copyFunc.empty()) {
       // Play dice
       if (!trial(engine)) {
-        DEBUG(errs() << "\t\tSkipping: Bernoulli trial failed\n");
+        DEBUG(errs() << "\tSkipping: Bernoulli trial failed\n");
         continue;
       }
     } else {
       if (std::find(funcListStart, funcListEnd, F.getName()) == funcListEnd) {
-          DEBUG(errs() << "\tFunction not requested -- skipping\n");
-          continue;
-        }
+        DEBUG(errs() << "\tFunction not requested -- skipping\n");
+        continue;
+      }
     }
+    DEBUG(errs() << "\tCloning\n");
+    cloneList.push_back(&F);
+  }
+
+  for (Function *F : cloneList){
+    hasBeenModified |= true;
+
+    // Refer to
+    // https://github.com/llvm-mirror/llvm/blob/release_34/lib/Transforms/Utils/CloneFunction.cpp#L162
+    std::vector<Type *> ArgTypes;
+    ValueToValueMapTy VMap;
+    for (Function::const_arg_iterator I = F->arg_begin(), E = F->arg_end();
+         I != E; ++I)
+      ArgTypes.push_back(I->getType());
+    FunctionType *FTy =
+        FunctionType::get(F->getFunctionType()->getReturnType(), ArgTypes,
+                          F->getFunctionType()->isVarArg());
+
+    Twine cloneName("");
+    DEBUG(cloneName = cloneName.concat(F->getName()).concat("_clone"));
+    Function *clone = Function::Create(FTy, F->getLinkage(), cloneName, &M);
+
+    Function::arg_iterator DestI = clone->arg_begin();
+    for (Function::const_arg_iterator I = F->arg_begin(), E = F->arg_end();
+         I != E; ++I) {
+      DestI->setName(I->getName()); // Copy the name over...
+      VMap[I] = DestI++;            // Add mapping to VMap
+    }
+    SmallVector<ReturnInst *, 8> Returns; // Ignore returns cloned.
+    CloneFunctionInto(clone, F, VMap, true, Returns);
   }
 
   return hasBeenModified;
 }
 
 char Copy::ID = 0;
-static RegisterPass<Copy>
-X("copy", "Copy function pass", false, false);
+static RegisterPass<Copy> X("copy", "Copy function pass", false, false);
