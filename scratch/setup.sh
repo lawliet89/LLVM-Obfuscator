2f80cbad8cc944b0517b26ab9dafa05d0ed8501f
Yong Wen Chua <yongwen.chua10@imperial.ac.uk>
2014-05-14 14:20:24 +0100

    Add some progress indicators to setup script

--- scratch/setup.sh
+++ scratch/setup.sh
@@ -14,10 +14,15 @@ fi
 
 
 mkdir -p $BUILD_DIR
+echo "================= Configuring LLVM ==============="
 (cd $BUILD_DIR && "$LLVM_PATH/configure" "$CONFIGURE_FLAGS")
+echo "================= Configuring Project ==============="
 (cd "$BUILD_DIR/projects" && mkdir -p LLVM-Obfuscator \
     && cd LLVM-Obfuscator && ../../../../configure "$CONFIGURE_FLAGS")
+echo "================= Building LLVM ==============="
 (cd $BUILD_DIR && make -j 4)
+echo "================= Building Project ==============="
 (cd "$BUILD_DIR/projects/LLVM-Obfuscator" && make -j 4)
 mkdir -p test
+make
 echo "Built and setup"
