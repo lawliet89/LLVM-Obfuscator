20dca7ecf889ac52ff44ce066015ef505cbb0332
Yong Wen Chua <yongwen.chua10@imperial.ac.uk>
2014-05-22 12:18:19 +0100

    More dynamic scheduling

--- lib/Transform/schedule.cpp
+++ lib/Transform/schedule.cpp
@@ -18,15 +18,141 @@
 #include "llvm/LinkAllPasses.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/Support/CommandLine.h"
+#include <vector>
 
 using namespace llvm;
 
-static cl::opt<bool> noObfSchedule(
-    "noObfSchedule", cl::init(false),
-    cl::desc("Do not schedule Obfuscation passes"));
+static cl::opt<bool>
+    noObfSchedule("noObfSchedule", cl::init(false),
+                  cl::desc("Do not schedule Obfuscation passes"));
 
-// Schedue the passes
-// http://homes.cs.washington.edu/~bholt/posts/llvm-quick-tricks.html
+static cl::opt<bool> trivialObfuscation(
+    "trivialObfuscation", cl::init(false),
+    cl::desc(
+        "Only scheudle trivial obfuscation passes that do not modify the CFG"));
+
+enum ScheduleOptions {
+  copyPass,
+  inlineFunctionPass,
+  bogusCFPass,
+  loopBCFPass,
+  opaquePredicatePass,
+  replaceInstructionPass,
+  flattenPass,
+  cleanupPass,
+  identifierRenamerPass
+};
+
+cl::list<ScheduleOptions> ObfuscationList(
+    cl::desc("Available Obfuscations for scheduling:"),
+    cl::values(clEnumVal(copyPass, "Copy Function"),
+               clEnumVal(inlineFunctionPass, "Inline function calls"),
+               clEnumVal(bogusCFPass, "Insert bogus control flow"),
+               clEnumVal(loopBCFPass, "Loop bogus control flow"),
+               clEnumVal(opaquePredicatePass, "Create opaque predicates"),
+               clEnumVal(replaceInstructionPass, "Replace Instruction"),
+               clEnumVal(flattenPass, "Flatten cotnrol flow"),
+               clEnumVal(cleanupPass, "Cleanup"),
+               clEnumVal(identifierRenamerPass, "Rename identifiers"),
+               clEnumValEnd));
+
+namespace {
+
+std::vector<Pass *> getPasses() {
+  std::vector<Pass *> passes;
+
+  if (trivialObfuscation) {
+    passes.push_back(new Copy());
+    passes.push_back(new InlineFunctionPass());
+    passes.push_back(new CleanupPass());
+    passes.push_back(new IdentifierRenamer());
+  } else if (!ObfuscationList.empty()) {
+    // Then we should schedule some passes required by the things below
+    passes.push_back(createDemoteRegisterToMemoryPass());
+
+    for (auto option : ObfuscationList) {
+      switch (option) {
+      case copyPass:
+        passes.push_back(new Copy());
+        break;
+      case inlineFunctionPass:
+        passes.push_back(new InlineFunctionPass());
+        break;
+      case bogusCFPass:
+        passes.push_back(new BogusCF());
+        break;
+      case loopBCFPass:
+        passes.push_back(createLoopSimplifyPass());
+        passes.push_back(new LoopBogusCF());
+        break;
+      case opaquePredicatePass:
+        passes.push_back(new OpaquePredicate());
+        break;
+      case replaceInstructionPass:
+        passes.push_back(new ReplaceInstruction());
+        break;
+      case flattenPass:
+        passes.push_back(new Flatten());
+        break;
+      case cleanupPass:
+        passes.push_back(new CleanupPass());
+        break;
+      case identifierRenamerPass:
+        passes.push_back(new IdentifierRenamer());
+        break;
+      default:
+        llvm_unreachable("Unknown option set");
+      }
+
+      // Clean up pass
+      passes.push_back(createPromoteMemoryToRegisterPass()); // Fix PHI
+                                                             // demotions
+      passes.push_back(createCFGSimplificationPass());       // Further cleanups
+    }
+  } else {
+
+    // Default Pass Set
+    // Demote PHIs to memory for ease of analysis
+    passes.push_back(createDemoteRegisterToMemoryPass());
+
+    // First batch of passes are trivial passes and should be run first to
+    // "maximise confusion" that the later passes will introduce
+    passes.push_back(new Copy());
+    passes.push_back(new InlineFunctionPass());
+
+    // Second batch of passes deal with introducing new control flow paths
+    // These passes will insert stub 1.00 == 1.00 branches
+    // which will be cleaned up by the OpaquePredicate pass
+    // OpaquePredicate pass MUST be run before Flatten Pass because Flatten
+    // will REMOVE the original branch instructions
+    passes.push_back(new BogusCF());
+    passes.push_back(createLoopSimplifyPass());
+    passes.push_back(new LoopBogusCF());
+    passes.push_back(new OpaquePredicate());
+
+    // The next pass will obfuscated unreachable blocks by introducing junk
+    passes.push_back(new ReplaceInstruction());
+
+    // Flatten the control flow
+    passes.push_back(new Flatten());
+
+    // Clean ups
+    passes.push_back(new CleanupPass()); // Remove stray metadata left over from
+                                         // passes
+    passes.push_back(createPromoteMemoryToRegisterPass()); // Fix PHI demotions
+    passes.push_back(createCFGSimplificationPass());       // Further cleanups
+
+    passes.push_back(new IdentifierRenamer());
+    // passes.push_back(createStripDebugDeclarePass());
+    // passes.push_back(createStripDeadDebugInfoPass());
+  }
+
+  return passes;
+}
+}
+
+  // Schedue the passes
+  // http://homes.cs.washington.edu/~bholt/posts/llvm-quick-tricks.html
 static RegisterStandardPasses Y(PassManagerBuilder::EP_OptimizerLast,
                                 [](const PassManagerBuilder &,
                                    PassManagerBase &PM) {
@@ -35,36 +161,9 @@ static RegisterStandardPasses Y(PassManagerBuilder::EP_OptimizerLast,
     return;
   }
 
-  // Demote PHIs to memory for ease of analysis
-  PM.add(createDemoteRegisterToMemoryPass());
+  std::vector<Pass *> passes = getPasses();
 
-  // First batch of passes are trivial passes and should be run first to
-  // "maximise confusion" that the later passes will introduce
-  PM.add(new Copy());
-  PM.add(new InlineFunctionPass());
-
-  // Second batch of passes deal with introducing new control flow paths
-  // These passes will insert stub 1.00 == 1.00 branches
-  // which will be cleaned up by the OpaquePredicate pass
-  // OpaquePredicate pass MUST be run before Flatten Pass because Flatten
-  // will REMOVE the original branch instructions
-  PM.add(new BogusCF());
-  PM.add(createLoopSimplifyPass());
-  PM.add(new LoopBogusCF());
-  PM.add(new OpaquePredicate());
-
-  // The next pass will obfuscated unreachable blocks by introducing junk
-  PM.add(new ReplaceInstruction());
-
-  // Flatten the control flow
-  PM.add(new Flatten());
-
-  // Clean ups
-  PM.add(new CleanupPass()); // Remove stray metadata left over from passes
-  PM.add(createPromoteMemoryToRegisterPass()); // Fix PHI demotions
-  PM.add(createCFGSimplificationPass()); // Further cleanups
-
-  PM.add(new IdentifierRenamer());
-  // PM.add(createStripDebugDeclarePass());
-  // PM.add(createStripDeadDebugInfoPass());
+  for (Pass *pass : passes) {
+    PM.add(pass);
+  }
 });
